eb848f4bbfc8f7837a4d5ab6691bcb566af23cf2
Guelito1 <mflorescacravens@gmail.com>
2019-09-30 14:16:43 -0700

    progress with material-ui

--- src/App.jsx
+++ src/App.jsx
@@ -15,82 +15,58 @@ import Runtime from './Runtime.png';
 import TheHacks from './TheHacks.png';
 import Mars from './Mars.png';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
+import Button from '@material-ui/core/Button';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/core/styles';
+import Icon from '@material-ui/core/Icon';
+import Divider from '@material-ui/core/Divider';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Drawer from '@material-ui/core/Drawer';
+import MenuIcon from '@material-ui/icons/Menu';
+import IconButton from '@material-ui/core/IconButton';
 
+const drawerWidth = 240;
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
 
-function App() {
+export default function CenteredGrid() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <img src={Miguel} alt="Miguel Floresca-Cravens" className="self"/>
-      <nav>
-        <ul>
-          <AnchorLink className='list' href="#About">About Me</AnchorLink>
-          <AnchorLink className='list' href="#Contact">Contact</AnchorLink>
-          <AnchorLink className='list' href="#Skills">Skills</AnchorLink>
-          <AnchorLink className='list' href="#Projects">Projects</AnchorLink>
-        </ul>
-      </nav>
-      <table className='trans'></table>
-      <section className="sectionArea">
-        <h1 className="about" id="About">About Me</h1>
-        <div className="shortBio">
-          <h2 className="Hello">Hello, my name is Miguel!</h2>
-          <h3>I am a full stack developer with background in finance. I have learned many valuable skills that
-            I look forward to transitioning in to software development!
-          </h3>
-        </div>
-      </section>
-      <br/>
-      <br/>
-      <section className="sectionArea"  id="Contact">
-        <h1>Contact</h1>
-        <div className="contact">
-          <div className="Github">
-            <a href="https://github.com/Guelito1">Miguel on Github</a>
-            <img href="https://github.com/Guelito1" src="" alt=""/>
-          </div>
-          <div className="LinkedIn">
-            <a href="https://www.linkedin.com/in/miguelfloresca-cravens/">Miguel on LinkedIn</a>
-            <img href="https://www.linkedin.com/in/miguelfloresca-cravens/" src="" alt=""/>
-          </div>
-        </div>
-      </section>
-      <section className="sectionArea" id="Skills">
-      <h1>Skills</h1>
-        <div className="iconsList">
-          <img className="icons" src={ReactImg} alt="React"/>
-          <img className="icons" src={MongoDB} alt="MongoDB"/>
-          <img className="icons" src={JS} alt="JS"/>
-          <img className="icons" src={TS} alt="TypeScript"/>
-          <img className="icons" src={CSS} alt="CSS"/>
-          <img className="icons" src={HTML} alt="HTML"/>
-          <img className="icons" src={Sequelize} alt="Sequelize"/>
-          <img className="icons" src={NodeJS} alt="NodeJS"/>
-          <img className="icons" src={Postgres} alt="Postgres"/>
-          <img className="icons" src={Django} alt="Django"/>
-        </div>
-      </section>
-      <section className="sectionArea" id="Projects">
-        <h2>Projects</h2>
-        <div className="projectsList">
-          <div className="project">
-              <h3>Title: "Runtime Terror - Drink Dictionary"</h3>
-              <img className="projectPic" src={Runtime} alt=""/>
-              <p>Info: A drink information website that stores your favorite cocktails. Click here to see the site: <a href="https://desolate-coast-19668.herokuapp.com">Runtime-Terror on Heroku</a> or on Github <a href="https://github.com/Guelito1/project2">here</a></p>
-          </div>
-          <div className="project">
-            <h3>Title: "Mars"</h3>
-            <img className="projectPic" src={Mars} alt="Mars Project"/>
-            <p>Info: A site built to allow comments by users to interact with Mars rovers using NASA's Mars Rover API.</p>
-          </div>
-          <div className="project">
-            <h3>Title: "The Hacks"</h3>
-            <img className="projectPic" src={TheHacks} alt="The Hacks"/>
-            <p>Info: A collaborative social site where users can categorize Hacker News stories in a free form format. Click here to see the site: <a href="https://github.com/Guelito1/project2">Github repo</a></p>
-          </div>
-        </div>
-      </section>
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton 
+              edge="start" 
+              className={classes.menuButton} 
+              color="inherit" 
+              aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Miguel's Portfolio
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Grid container position={'relative'} spacing={3}>
+        <img item xs={12} style={{height: 250}} src={Miguel} alt="Miguel Floresca-Cravens"/>
+        <Grid item xs={12}>
+          <Button variant="contained" color='primary'>stuff</Button>
+        </Grid>
+      </Grid>
     </div>
   );
 }
-
-export default App;
