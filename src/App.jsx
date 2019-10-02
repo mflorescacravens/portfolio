cdf11038dd31182b67025b49f890a98cfb63c6f5
Guelito1 <mflorescacravens@gmail.com>
2019-10-01 19:01:05 -0700

    button functionality

--- src/App.jsx
+++ src/App.jsx
@@ -20,31 +20,60 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
-import TextField from '@material-ui/core/TextField';
-import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';
-import Icon from '@material-ui/core/Icon';
-import Divider from '@material-ui/core/Divider';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Drawer from '@material-ui/core/Drawer';
 import MenuIcon from '@material-ui/icons/Menu';
 import IconButton from '@material-ui/core/IconButton';
+import SendIcon from '@material-ui/icons/Send';
+import CallIcon from '@material-ui/icons/Call';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import Link from '@material-ui/core/Link';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
 
-const drawerWidth = 240;
+
 const useStyles = makeStyles(theme => ({
   root: {
-    flexGrow: 1
+    display: 'flex',
+    flexGrow: 1,
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
   },
   paper: {
     padding: theme.spacing(2),
     textAlign: 'center',
     color: theme.palette.text.secondary,
   },
+  gridList: {
+    width: 500,
+    height: 450,
+  },
+  icon: {
+    color: 'rgba(255, 255, 255, 0.5)',
+  },
+  rightIcon: {
+    marginLeft: theme.spacing(1),
+  },
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 }));
 
-export default function CenteredGrid() {
+export default function App() {
   const classes = useStyles();
+  const [modal, setModal] = React.useState(false);
 
+  const openModal = () => {
+    setModal(true);
+  }
+  const closeModal = () => {
+    setModal(false);
+  }
+  
   return (
     <div>
       <AppBar position="static">
@@ -57,15 +86,44 @@ export default function CenteredGrid() {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" className={classes.title}>
-            Miguel's Portfolio
+            Miguel Floresca-Cravens
           </Typography>
         </Toolbar>
       </AppBar>
-      <Grid container position={'relative'} spacing={3}>
-        <img item xs={12} style={{height: 250}} src={Miguel} alt="Miguel Floresca-Cravens"/>
+      <Grid container style={{padding: 10}} spacing={-10}>
+        <img item style={{height: 250}} xs={12} src={Miguel} alt="Miguel Floresca-Cravens"/>
+        <Typography xs={6}>Hello, my name is Miguel! I am a full stack developer with background in finance. I have a passion for music and enjoy solving problems. I look forward to using my skills in future projects!</Typography>
+      </Grid>
+      <Grid container>
         <Grid item xs={12}>
-          <Button variant="contained" color='primary'>stuff</Button>
+          <Button href="mailto:mflorescacravens@gmail.com" className={classes.rightIcon} variant="contained" color='primary'>
+              Contact Miguel via Email
+          </Button>
         </Grid>
+        <Button item onClick={openModal} xs={5} className={classes.rightIcon} variant="contained" color='primary'>
+          Contact Miguel via Phone
+          <CallIcon/>
+        </Button>
+        <Button item href="https://www.linkedin.com/in/miguelfloresca-cravens/" xs={5} className={classes.rightIcon} variant="contained" color='primary'>
+          Contact Miguel via LinkedIn
+          <LinkedInIcon />
+        </Button>
+        <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={modal}
+        onClose={closeModal}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        >
+          <Fade in={modal}>
+            <div className={classes.paper}>
+              <h2 id="transition-modal-title">Miguel's Phone Number</h2>
+              <Typography id="transition-modal-description">(206) 941-1312</Typography>
+            </div>
+          </Fade>
+        </Modal>
       </Grid>
     </div>
   );
