efd9a79fb4d9ad744eb7e50e834768d607c3d7c4
Guelito1 <mflorescacravens@gmail.com>
2020-01-26 21:09:31 -0800

    nav bar styling fixed

--- src/AppBar.jsx
+++ src/AppBar.jsx
@@ -5,6 +5,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button'
 import { Link } from 'react-router-dom';
 
 
@@ -22,7 +23,8 @@ const useStyles = makeStyles(theme => ({
     nav: {
       flexGrow: 1,
       justify: 'center',
-      marginLeft: theme.spacing(3)
+      marginLeft: theme.spacing(3),
+
     }
 }));
 
@@ -43,19 +45,19 @@ export default function AppBars() {
             Miguel Floresca-Cravens
           </Typography>
           <Link to='/'>
-            <Typography className={classes.nav}>Home</Typography>
+            <Button variant='outlined'  size='large' className={classes.nav}>Home</Button>
           </Link>
           <Link to='/contact'>
-            <Typography className={classes.nav}>Contact</Typography>
+            <Button variant='outlined'  size='large' className={classes.nav}>Contact</Button>
           </Link>
           <Link to='/skills'>
-            <Typography className={classes.nav}>Skills</Typography>
+            <Button variant='outlined'  size='large' className={classes.nav}>Skills</Button>
           </Link>
           <Link to='/projects'>
-            <Typography className={classes.nav}>Projects</Typography>
+            <Button variant='outlined'  size='large' className={classes.nav}>Projects</Button>
           </Link>
           <Link to='/resume'>
-            <Typography className={classes.nav}>Resume</Typography>
+            <Button variant='outlined'  size='large' className={classes.nav}>Resume</Button>
           </Link>
         </Toolbar>
       </AppBar>
